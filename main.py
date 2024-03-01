d7640bb40c5292f07414e49f1526107ed4be4377
Hugo0133 <hugo.jimgarcia@gmail.com>
2024-03-01 11:37:26 +0100

    proceso poco a poco

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from libraries.rag import RAG
 
 app = Flask(__name__)
+
+# ===================================
+# PROVIONAL
+input_path = "vector_db"
+rag = RAG(vectordb_path=input_path)
 
 @app.route('/')
 def hello_world():
